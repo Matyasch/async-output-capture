9a30c0dd3e8aae66304cf1855da398ca40e1fc56
Matyasch <schubertmatyas@gmail.com>
2020-12-25 13:35:10 +0100

    Make unit tests compatible with unittest library

--- tests/test_stdout.py
+++ tests/test_stdout.py
@@ -2,6 +2,7 @@
 
 import sys
 from time import sleep
+import unittest
 
 from async_output_capture import StdoutCaptureProcess
 
@@ -12,30 +13,13 @@ def sleepy_printer(text):
         sleep(0.1)
 
 
-def test_wait_sleepy_print_to_finish_and_check_after():
-    p = StdoutCaptureProcess(target=sleepy_printer, args=('test',))
-    p.start()
-    p.join()
+class TestStdOutWaiting(unittest.TestCase):
+    def test_wait_sleepy_print_to_finish_and_check_after(self):
+        p = StdoutCaptureProcess(target=sleepy_printer, args=('test',))
+        p.start()
+        p.join()
 
-    # Check after function is done
-    outputs = ''
-    while True:
-        val = p.get_output()
-        if val:
-            outputs += val
-        else:
-            break
-
-    assert outputs.splitlines() == ['{}. test'.format(i) for i in range(50)]
-
-
-def test_wait_sleepy_print_to_finish_and_check_during_and_after():
-    p = StdoutCaptureProcess(target=sleepy_printer, args=('test',))
-    p.start()
-
-    # Check during function execution
-    curr_output = 0
-    for _ in range(10):
+        # Check after function is done
         outputs = ''
         while True:
             val = p.get_output()
@@ -43,22 +27,40 @@ def test_wait_sleepy_print_to_finish_and_check_during_and_after():
                 outputs += val
             else:
                 break
-        for output in outputs.splitlines():
-            assert output == '{}. test'.format(curr_output)
-            curr_output += 1
-        sleep(0.2)
 
-    p.join()
-    # Check after function is done
-    outputs = ''
-    while True:
-        val = p.get_output()
-        if val:
-            outputs += val
-        else:
-            break
+        self.assertEqual(outputs.splitlines(), ['{}. test'.format(i) for i in range(50)])
 
-    assert outputs.splitlines() == ['{}. test'.format(i) for i in range(curr_output, 50)]
+
+    def test_wait_sleepy_print_to_finish_and_check_during_and_after(self):
+        p = StdoutCaptureProcess(target=sleepy_printer, args=('test',))
+        p.start()
+
+        # Check during function execution
+        curr_output = 0
+        for _ in range(10):
+            outputs = ''
+            while True:
+                val = p.get_output()
+                if val:
+                    outputs += val
+                else:
+                    break
+            for output in outputs.splitlines():
+                self.assertEqual(output, '{}. test'.format(curr_output))
+                curr_output += 1
+            sleep(0.2)
+
+        p.join()
+        # Check after function is done
+        outputs = ''
+        while True:
+            val = p.get_output()
+            if val:
+                outputs += val
+            else:
+                break
+
+        self.assertEqual(outputs.splitlines(), ['{}. test'.format(i) for i in range(curr_output, 50)])
 
 
 def instant_printer(text):
@@ -66,30 +68,13 @@ def instant_printer(text):
         sys.stdout.write('{}. {}\n'.format(i, text))
 
 
-def test_wait_instant_print_to_finish_and_check_after():
-    p = StdoutCaptureProcess(target=instant_printer, args=('test',))
-    p.start()
-    p.join()
+class TestStdOutInstant(unittest.TestCase):
+    def test_wait_instant_print_to_finish_and_check_after(self):
+        p = StdoutCaptureProcess(target=instant_printer, args=('test',))
+        p.start()
+        p.join()
 
-    # Check after function is done
-    outputs = ''
-    while True:
-        val = p.get_output()
-        if val:
-            outputs += val
-        else:
-            break
-
-    assert outputs.splitlines() == ['{}. test'.format(i) for i in range(2000)]
-
-
-def test_wait_instant_print_to_finish_and_check_during_and_after():
-    p = StdoutCaptureProcess(target=instant_printer, args=('test',))
-    p.start()
-
-    # Check during function execution
-    curr_output = 0
-    for _ in range(10):
+        # Check after function is done
         outputs = ''
         while True:
             val = p.get_output()
@@ -97,19 +82,37 @@ def test_wait_instant_print_to_finish_and_check_during_and_after():
                 outputs += val
             else:
                 break
-        for output in outputs.splitlines():
-            assert output == '{}. test'.format(curr_output)
-            curr_output += 1
-        sleep(0.2)
 
-    p.join()
-    # Check after function is done
-    outputs = ''
-    while True:
-        val = p.get_output()
-        if val:
-            outputs += val
-        else:
-            break
+        self.assertEqual(outputs.splitlines(), ['{}. test'.format(i) for i in range(2000)])
 
-    assert outputs.splitlines() == ['{}. test'.format(i) for i in range(curr_output, 2000)]
+
+    def test_wait_instant_print_to_finish_and_check_during_and_after(self):
+        p = StdoutCaptureProcess(target=instant_printer, args=('test',))
+        p.start()
+
+        # Check during function execution
+        curr_output = 0
+        for _ in range(10):
+            outputs = ''
+            while True:
+                val = p.get_output()
+                if val:
+                    outputs += val
+                else:
+                    break
+            for output in outputs.splitlines():
+                self.assertEqual(output, '{}. test'.format(curr_output))
+                curr_output += 1
+            sleep(0.2)
+
+        p.join()
+        # Check after function is done
+        outputs = ''
+        while True:
+            val = p.get_output()
+            if val:
+                outputs += val
+            else:
+                break
+
+        self.assertEqual(outputs.splitlines(), ['{}. test'.format(i) for i in range(curr_output, 2000)])
